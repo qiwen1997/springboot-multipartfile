62bc42b3b8bc4577b95bd6cb0b13b85ca4bec8ae
qiwen <971449438@qq.com>
2019-06-24 14:45:41 +0800

    two,浏览器兼容下载

--- src/main/java/hello/FileUploadController.java
+++ src/main/java/hello/FileUploadController.java
@@ -1,8 +1,16 @@
 package hello;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +21,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -53,6 +62,82 @@ public class FileUploadController {
     return "uploadForm";
   }
 
+  //实现Spring Boot 的文件下载功能，映射网址为/download
+  @RequestMapping("/download")
+  public String downloadFile(HttpServletRequest request,
+      HttpServletResponse response) throws UnsupportedEncodingException {
+
+    // 获取指定目录下的第一个文件
+    File scFileDir = new File("E://yonyou/IDEAspace/springboot-multipartfile/upload-dir");
+    File TrxFiles[] = scFileDir.listFiles();
+    System.out.println(TrxFiles[0]);
+    String fileName = TrxFiles[0].getName(); //下载的文件名
+
+    // 如果文件名不为空，则进行下载
+    if (fileName != null) {
+      //设置文件路径
+      String realPath = "E://yonyou/IDEAspace/springboot-multipartfile/upload-dir";
+      File file = new File(realPath, fileName);
+
+      // 如果文件名存在，则进行下载
+      if (file.exists()) {
+
+        // 配置文件下载
+        response.setHeader("content-type", "application/octet-stream");
+
+        //解决浏览器兼容
+        response.setContentType("application/octet-stream");
+        boolean isMSIE = HttpUtils.isMSBrowser(request);
+        if (isMSIE) {
+          fileName = URLEncoder.encode(fileName, "UTF-8");
+        } else {
+          fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+        }
+        response.setHeader("Content-disposition", "attachment;filename=\"" + fileName + "\"");
+
+        // 实现文件下载
+        byte[] buffer = new byte[1024];
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        try {
+          fis = new FileInputStream(file);
+          bis = new BufferedInputStream(fis);
+          OutputStream os = response.getOutputStream();
+          int i = bis.read(buffer);
+          while (i != -1) {
+            os.write(buffer, 0, i);
+            i = bis.read(buffer);
+          }
+          System.out.println("Download the song successfully!");
+        }
+        catch (Exception e) {
+          System.out.println("Download the song failed!");
+        }
+        finally {
+          if (bis != null) {
+            try {
+              bis.close();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+          if (fis != null) {
+            try {
+              fis.close();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+
+
+
+
   // 文件下载
   // 正则表达式匹配, 语法: {varName:regex} 前面式变量名，后面式表达式
   // 匹配出现过一次或多次.的字符串 如: "xyz.png"
@@ -61,6 +146,7 @@ public class FileUploadController {
   public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
     // 根据文件名读取文件
     Resource file = storageService.loadAsResource(filename);
+
     // @ResponseBody 用于直接返回结果(自动装配)
     // ResponseEntity 可以定义返回的 HttpHeaders 和 HttpStatus (手动装配)
     // ResponseEntity.ok 相当于设置 HttpStatus.OK (200)
